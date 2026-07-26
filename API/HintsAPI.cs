6879a87cf2c005994731db7614534f4fda5fd7ea
MisterT13 <mistert131@gmx.de>
2026-07-27 00:32:18 +0200

    customhints

---
+++ API/HintsAPI.cs
@@ -0,0 +1,115 @@
+using System.Collections.Generic;
+using LabApi.Features.Extensions;
+using NebMainPlugin.API.Enums;
+using NebMainPlugin.Systems.CustomHints;
+using PlayerRoles;
+using Respawning.Objectives;
+using UnityEngine;
+using Player = LabApi.Features.Wrappers.Player;
+
+namespace NebMainPlugin.API{
+
+    public static class HintsAPI
+    {
+        public static string GetRoleColor(Player player)
+        {
+            return "#"+ ColorUtility.ToHtmlStringRGB(player.Role.GetRoleColor());
+        }
+
+        public static string GetRoleName(Player player)
+        {
+            //this is fucking weird fix this
+            switch (player.Role)
+            {
+                case RoleTypeId.AlphaFlamingo:
+                    return "SCP-1507 Alpha";
+                case RoleTypeId.Flamingo:
+                    return "SCP-1507";
+                case RoleTypeId.ChaosFlamingo:
+                    return "SCP-1507";
+                case RoleTypeId.NtfFlamingo:
+                    return "SCP-1507";
+                case RoleTypeId.ZombieFlamingo:
+                    return "SCP-1507";
+                case RoleTypeId.CustomRole:
+                    return player.Role.GetFullName();
+                case RoleTypeId.ChaosConscript:
+                    return "Chaos Rekrut";
+                case RoleTypeId.ChaosMarauder:
+                    return "Chaos Marodeur";
+                case RoleTypeId.ChaosRepressor:
+                    return "Chaos Unterdrücker";
+                case RoleTypeId.ChaosRifleman:
+                    return "Chaos Gewehrschütze";
+                case RoleTypeId.ClassD:
+                    return "Klasse-D";
+                case RoleTypeId.Destroyed:
+                    return "Destroyed";
+                case RoleTypeId.FacilityGuard:
+                    return "Sicherheitspersonal";
+                case RoleTypeId.NtfCaptain:
+                    return "NTF Hauptmann";
+                case RoleTypeId.NtfPrivate:
+                    return "NTF Gefreiter";
+                case RoleTypeId.NtfSergeant:
+                    return "NTF Unteroffizier";
+                case RoleTypeId.NtfSpecialist:
+                    return "NTF Spezialist";
+                case RoleTypeId.Scientist:
+                    return "Wissenschaftler";
+                case RoleTypeId.Tutorial:
+                    return "Tutorial";
+                case RoleTypeId.Overwatch:
+                    return "Overwatch";
+                case RoleTypeId.Filmmaker:
+                    return "Filmmacher";
+                case RoleTypeId.Spectator:
+                    return "Zuschauer";
+                case RoleTypeId.None:
+                    return "None";
+                case RoleTypeId.Scp049:
+                    return "SCP-049";
+                case RoleTypeId.Scp0492:
+                    return "SCP-049-2";
+                case RoleTypeId.Scp079:
+                    return "SCP-079";
+                case RoleTypeId.Scp096:
+                    return "SCP-096";
+                case RoleTypeId.Scp106:
+                    return "SCP-106";
+                case RoleTypeId.Scp173:
+                    return "SCP-173";
+                case RoleTypeId.Scp939:
+                    return "SCP-939";
+                case RoleTypeId.Scp3114:
+                    return "SCP-3114";
+                default:
+                    return player.Role.GetFullName();
+            }
+        }
+        public static void AddHint(Player player, string text, float duration)
+        {
+            var hint = new Systems.CustomHints.Hints.HintData(text, duration);
+            if (!Systems.CustomHints.Hints.playerHints.ContainsKey(player))
+            {
+                Systems.CustomHints.Hints.playerHints.Add(player,new  List<Systems.CustomHints.Hints.HintData>());
+            } 
+            Systems.CustomHints.Hints.playerHints[player].Add(hint);
+        }
+
+        public static string GetPlayerName(Player player,int? maxChars = null)
+        {
+            string name = player.Nickname;
+            if (maxChars == null)
+            {
+                return name;
+            }
+
+            if (name.Length > maxChars)
+            {
+                name = name.Remove(maxChars.Value,name.Length-maxChars.Value)+ "...";
+            }
+            return name;
+        }
+    }
+}
---
+++ Systems/CustomHints/EventHandlers.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using System.Linq;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Handlers;
+using LabApi.Features.Wrappers;
+using MEC;
+using PlayerRoles;
+using PlayerStatsSystem;
+using UnityEngine;
+
+namespace NebMainPlugin.Systems.CustomHints{
+
+    public class EventHandlers
+    {
+        public static Dictionary<Player,int> Kills = new();
+        public static void Enable()
+        {
+            PlayerEvents.Joined += OnJoined;
+            PlayerEvents.ChangingRole += OnChangingRole;
+            PlayerEvents.Death += OnKill;
+            PlayerEvents.ChangedSpectator += OnChangingSpec;
+            ServerEvents.RoundRestarted += OnRoundRestart;
+        }
+
+        public static void Disable()
+        {
+            PlayerEvents.Joined -= OnJoined;
+            PlayerEvents.ChangingRole -= OnChangingRole;
+            PlayerEvents.Death -= OnKill;
+            PlayerEvents.ChangedSpectator -= OnChangingSpec;
+            ServerEvents.RoundRestarted -= OnRoundRestart;
+
+        }
+
+        private static void OnJoined(PlayerJoinedEventArgs ev)
+        {
+            Hints.RefreshTps(ev.Player);
+            Hints.RefreshTime(ev.Player);
+            Hints.RefreshRoundtime(ev.Player);
+            Hints.RefreshKills(ev.Player);
+            Hints.RefreshSpectators(ev.Player);
+            Hints.RefreshServerName(ev.Player);
+            Hints.RefreshRole(ev.Player);
+            Hints.RegisterHintDisplay(ev.Player);
+        }
+
+        private static void OnChangingRole(PlayerChangingRoleEventArgs ev)
+        { 
+            Hints.RefreshSpectators(ev.Player);
+            
+            //Delayed because the event is called before the role gets changed
+            Timing.CallDelayed(0.01f,() =>
+            {
+                Hints.RefreshRole(ev.Player);
+                Hints.RefreshSCPHints(ev.Player);
+                Hints.RegisterHintDisplay(ev.Player);
+                Hints.RefreshKills(ev.Player);
+            });
+            
+        }
+
+        private static void OnKill(PlayerDeathEventArgs ev)
+        {
+            if (ev.DamageHandler is UniversalDamageHandler universal 
+                && universal.TranslationId == DeathTranslations.PocketDecay.Id)
+            {
+                foreach (var scp106 in Player.List.Where(pl => pl.Role == RoleTypeId.Scp106))
+                {
+                    AddKill(ev.Player);
+                }
+            }
+
+            if (ev.Attacker != null && ev.Attacker != ev.Player)
+            {
+                AddKill(ev.Attacker);
+            }
+        }
+
+        private static void AddKill(Player p)
+        {
+            if (Kills.TryGetValue(p, out var kills))
+            {
+                Kills[p] = kills + 1;
+            }
+            else
+            {
+                Kills.Add(p, 1);
+            }
+            Hints.RefreshKills(p);
+        }
+
+        private static void OnChangingSpec(PlayerChangedSpectatorEventArgs ev)
+        {
+            Timing.CallDelayed(0.01f,() =>
+            {
+                Hints.RefreshKills(ev.Player);
+                Hints.RefreshRole(ev.Player);
+            });
+        }
+
+        private static void OnRoundRestart()
+        {
+            Hints.playerHints.Clear();
+        }
+    }
+}
